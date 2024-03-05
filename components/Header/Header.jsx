542a5eaf5df33c5ccdd92ef01bdb27719a5c3aaf
Md Antor <md.antor1155@gmail.com>
2024-03-06 05:01:32 +0600

    header component made and top part done

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,64 @@
+import Image from "next/image";
 
 const Header = () => {
     return (
         <header>
-            <div className='bg-black border-2 text-orange-300'>
-                <h2 >sdflj</h2>
-            {/* <img src="/icons/twitter.svg" alt="" /> */}
+            <div className='h-14 bg-black flex justify-evenly items-center text-sm font-bold text-white'>
+                <div>
+
+                    <p className="inline-flex mr-5">
+                        <Image
+                            src="/icons/phone.svg"
+                            width={16}
+                            height={16}
+                            className="mr-2"
+                        />
+
+                        (223) 555-0118
+                    </p>
+                    <p className="inline-flex">
+                        <Image
+                            src="/icons/email.svg"
+                            width={16}
+                            height={16}
+                            className="mr-2"
+                        />
+
+                        tospinomall@example.com
+                    </p>
+                </div>
+
+                <p>
+                    Follow Us  and get a chance to win 80% off
+                </p>
+
+                <p className="inline-flex items-center gap-2">
+                    Follow us :
+                    <Image
+                        src="/icons/instagram.svg"
+                        width={16}
+                        height={16}
+                    />
+                    <Image
+                        src="/icons/youtube.svg"
+                        width={16}
+                        height={16}
+                    />
+                    <Image
+                        src="/icons/facebook.svg"
+                        width={16}
+                        height={16}
+                    />
+
+                    <Image
+                        src="/icons/twitter.svg"
+                        width={16}
+                        height={16}
+                    />
+                </p>
+
             </div>
-            
+
         </header>
     );
 };
