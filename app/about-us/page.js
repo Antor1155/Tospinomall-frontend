b1174273bd10eea3d1b0c5d4289b9992709b8e01
Md Antor <md.antor1155@gmail.com>
2024-03-08 04:02:49 +0600

    about us page done

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -50,7 +50,7 @@ const page = () => {
                     <BusinessImgCarousel />
                 </div>
 
-                <div className="flex flex-col justify-center ps-8 order-1 md:order-2">
+                <div className="flex flex-col justify-center ps-0 md:ps-8 order-1 md:order-2">
                     <h1 className="text-5xl font-semibold">Our Business</h1>
                     <p className="my-8">
                         TospinoMall is a leading e-commerce platform in Ghana. It was founded in 2018. TospinoMall has been growing rapidly since its launch in January 2021. The 2021 Shenzhen cross-border e-commerce trading expo was a big success for TospinoMall. Many popular international brands, local brands, and entrepreneurs are associated with Tospinomol. According to a leading web analytics company, TospinoMall is ranked 4th in Ghana based on downloads and users on the Google Play Store.
@@ -124,31 +124,31 @@ const page = () => {
             </section>
 
             {/* stats section  */}
-            <section className="bg-[url('/images/statsBackground.png')] bg-cover grid grid-cols-2 md:grid-cols-3 px-[5%] py-5">
+            <section className="bg-[url('/images/statsBackground.png')] bg-cover grid grid-cols-2 md:grid-cols-3 gap-5 px-[5%] py-5">
 
-                <div className="stat text-center text-lg w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black ">Visits on the website in 2022</div>
+                <div className=" text-center text-lg w-auto">
+                    <div className=" text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="-title text-black text-sm md:text-base">Visits on the website in 2022</div>
                 </div>
-                <div className="stat text-center text-xl w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black">Active Sellers</div>
+                <div className=" text-center text-xl w-auto">
+                    <div className="text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="text-black text-sm md:text-base ">Active Sellers</div>
                 </div>
-                <div className="stat text-center text-xl w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black">One transaction is done</div>
+                <div className=" text-center text-xl w-auto">
+                    <div className="text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="text-black text-sm md:text-base">One transaction is done</div>
                 </div>
-                <div className="stat text-center text-xl w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black">Orders in 2022</div>
+                <div className=" text-center text-xl w-auto">
+                    <div className="text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="text-black text-sm md:text-base">Orders in 2022</div>
                 </div>
-                <div className="stat text-center text-xl w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black">Products</div>
+                <div className=" text-center text-xl w-auto">
+                    <div className="text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="text-black text-sm md:text-base">Products</div>
                 </div>
-                <div className="stat text-center text-xl w-auto">
-                    <div className="stat-value">100k+</div>
-                    <div className="stat-title text-black">Active Consumers across Africa</div>
+                <div className=" text-center text-xl w-auto">
+                    <div className="text-3xl md:text-5xl font-bold my-5">100k+</div>
+                    <div className="text-black text-sm md:text-base">Active Consumers across Africa</div>
                 </div>
 
             </section>
@@ -156,7 +156,7 @@ const page = () => {
             {/* our purpose section  */}
             <section className="my-16 px-[5%]">
                 <div className="text-center w-full md:w-2/3 mx-auto mb-10 md:mb-16">
-                    <h2 className="font-semibold text-4xl mb-5">Our Purpose</h2>
+                    <h2 className="font-semibold text-5xl mb-5">Our Purpose</h2>
                     <p>
                         Our purpose is simple but significant. We are working to improve lives via fantastic shopping experiences. We are a community-driven platform that connects customers with high-quality products, trustworthy sellers, and creative solutions. From daily needs to exclusive finds, we strive to make shopping simple, enjoyable, and satisfying. With a focus on customer satisfaction and empowerment, we aim to build trust and develop long-term partnerships
                     </p>
@@ -176,6 +176,68 @@ const page = () => {
                             Our vision is to take TospinoMall higher by exceeding customer expectations. Revolutionizing online shopping in Ghana by providing a seamless and easy experience. Building a reliable platform for all retail and wholesale traders in Ghana, including consumers, through TospinoMall's vast supply chain and manufacturing.
                         </p>
                     </div>
+                </div>
+
+            </section>
+
+            {/* our supplier section  */}
+            <section className="px-[5%] pb-16 pt-5">
+                <h2 className="font-semibold text-5xl text-center mb-7">Our Supplier</h2>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
+                    <div className="order-2">
+                        <h3 className="text-3xl font-semibold mb-5">TospinoMall Logistics</h3>
+                        <p>
+                            TospinoMall Logistics is the best e-commerce fulfillment and express delivery system in Ghana. It is known as Tospino Express in Ghana, providing fast and secure delivery anywhere in Ghana. “Tospino Express” is always best for the quickest and safest delivery.
+                            Any local entrepreneur or small business in Ghana can easily join us. Through Tospino Express, they can deliver their product to the customer in the fastest and safest way.
+                        </p>
+                    </div>
+
+                    <Image
+                        src="/images/ourpurpose1.png"
+                        width="300"
+                        height="300"
+                        alt="TospinoMall Logistics"
+                        className="w-full order-1 md:order-3"
+                    />
+
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center my-10">
+
+                    <Image
+                        src="/images/ourpurpose2.png"
+                        width="300"
+                        height="300"
+                        alt="Tospino Wholesale"
+                        className="w-full"
+                    />
+
+                    <div >
+                        <h3 className="text-3xl font-semibold mb-5">Tospino Wholesale</h3>
+                        <p>
+                            Welcome to Tospino Wholesale, your premier source for high-quality LED lights in Africa. As a top wholesale distributor, we specialize in supplying energy-efficient LED lighting solutions to businesses and retailers across the continent. Our collection has a large selection of LED items that may be personalized to fit your lighting needs, including fixtures and bulbs. We are the premier option in Africa for wholesale lighting solutions because we are dedicated to delivering high-quality products at affordable rates. Explore our offerings today and enhance your business experience with Tospino Wholesale!
+                        </p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
+                    <div className="order-2">
+                        <h3 className="text-3xl font-semibold mb-5">Forever and Voor Electronics Co. Ltd</h3>
+                        <p>
+                            Explore the best in lighting innovation with "Forever and Voor Electronics Co. Ltd.", your premier wholesale supplier of high-quality lights manufactured in China. Specializing in LED, solar, and traditional lighting solutions, we cater to businesses across Africa, offering reliable products at wholesale prices. As a trusted partner, we ensure superior quality and durability, meeting the diverse needs of our clients. Illuminate your spaces efficiently and affordably with Forever and Voor Electronics Co. Ltd. Explore our vast range of lighting options today and easily brighten your business. Join with us for wholesale excellence in Africa.
+
+                        </p>
+                    </div>
+
+                    <Image
+                        src="/images/ourpurpose1.png"
+                        width="300"
+                        height="300"
+                        alt="Forever and Voor Electronics Co. Ltd"
+                        className="w-full order-1 md:order-3"
+                    />
+
                 </div>
 
             </section>
