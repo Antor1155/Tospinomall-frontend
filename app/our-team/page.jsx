1d4729d6864325696c6a7967a86e1215bcfbc3ae
Md Antor <md.antor1155@gmail.com>
2024-03-09 07:09:45 +0600

    our team page done

--- app/our-team/page.jsx
+++ app/our-team/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const page = () => {
     return (
         <main className="bg-off-white">
-            <section className="flex flex-wrap gap-7 px-0 md:px-[5%]">
+            <section className="flex flex-wrap md:flex-nowrap gap-7 px-0 md:px-[5%]">
 
                 <div className="w-full md:w-1/3 grid grid-rows-2 gap-4 items-stretch ">
 
@@ -155,6 +155,66 @@ const page = () => {
                     </div>
                 </div>
 
+            </section>
+
+            {/* other team members  */}
+            <section className="py-12 px-[5%]">
+                <h3 className="text-4xl font-bold text-center mb-12">Our Team</h3>
+
+                <div className="flex flex-wrap justify-between gap-5">
+                    {[...Array(12)].map(val => {
+                        return (
+                            <div className="flex items-center gap-5">
+                                <Image
+                                    src="/our-team/teamMember1.png"
+                                    width={150}
+                                    height={150}
+                                    alt="team members of tospino"
+                                />
+
+                                <div>
+                                    <h3 className="text-xl font-semibold">Jeff <br /> Kolesnikowicz</h3>
+                                    <p className="text-dim-text">Full-Stack Engineer</p>
+
+                                    <div className="flex gap-3 mt-3">
+                                        <Link href="">
+                                            <Image
+                                                src="/icons/facebookBlack.svg"
+                                                width={16}
+                                                height={17}
+                                                alt="facebook"
+                                                className="bg-tospino/50 rounded p-1"
+                                            />
+                                        </Link>
+
+                                        <Link href="">
+                                            <Image
+                                                src="/icons/instagramBlack.svg"
+                                                width={20}
+                                                height={20}
+                                                alt="instagram"
+                                                className="bg-tospino/50 rounded p-1"
+                                            />
+                                        </Link>
+
+                                        <Link href="">
+                                            <Image
+                                                src="/icons/linkedinBlack.svg"
+                                                width={20}
+                                                height={20}
+                                                alt="linkedin"
+                                                className="bg-tospino/50 rounded p-1"
+                                            />
+                                        </Link>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        )
+                    })}
+
+                </div>
             </section>
         </main>
     );
